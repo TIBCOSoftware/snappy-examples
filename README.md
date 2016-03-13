d46e050c58a38328f0a8a226d62c3eca1d9e43af
ymahajan <ymahajan@snappydata.io>
2016-03-13 09:08:28 +0530

    + log aggregator for ad analytics + log aggregator using Snappy, Spark, Cassandra and MemSql connector + random log generator using kafka which will be consumed by these four programs through spark streaming, kafka using avro format for serialization. + Added README.md and gradle dependencies to all the components + For Snappy it uses dependencies which are published locally, as we don't have latest snapshot published at remote location

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# snappy-examples
-Examples demonstrating SnappyData use cases and usage patterns for reference
+In order to run this example, we need to install the followings:
+
+1. Scala 2.10
+2. Apache Kafka
+3. SnappyData
+
+Please follow the below steps to run the example:
+
+Start Zookeeper:
+```
+bin/zookeeper-server-start.sh config/zookeeper.properties
+```
+Start Kafka Broker1:
+```
+bin/kafka-server-start.sh config/server1.properties
+```
+Start Kafka Broker2:
+```
+bin/kafka-server-start.sh config/server2.properties
+```
+Create a topic “adnetwork-topic”:
+```
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 8 --topic adnetwork-topic
+```
+
+Start generating and publishing logs to Kafka
+```
+./gradlew gen
+```
+
+Start SnappyData Locator:
+In snappy-commons/build-artifacts/scala-2.10/snappy/conf, create a file named locators and add following line : 
+```
+localhost -peer-discovery-port=10334 -dir=/tmp/locator -heap-size=1024m 
+```
+and run following command 
+```
+snappy-commons/build-artifacts/scala-2.10/snappy/sbin $ ./snappy-locators.sh start
+```
+
+Start SnappyData Servers:  
+In snappy-commons/build-artifacts/scala-2.10/snappy/conf, create a file named servers and add following two lines to create two servers: 
+```
+localhost -dir='/tmp/server1' -locators='localhost:10334' -heap-size=4096m -classpath='PATH_TO_GIT_CHECKOUT/ad_impression_log_aggregator/build/libs/LogAggregator-1.0-SNAPSHOT.jar'
+localhost -dir='/tmp/server2' -locators='localhost:10334' -heap-size=4096m -classpath='PATH_TO_GIT_CHECKOUT/ad_impression_log_aggregator/build/libs/LogAggregator-1.0-SNAPSHOT.jar'
+```
+and run following command 
+```
+snappy-commons/build-artifacts/scala-2.10/snappy/sbin $ ./snappy-servers.sh start
+
+```
+
+Start aggregation
+```
+./gradlew aggr
+```
---
+++ src/main/scala/io/snappydata/aggr/SnappyLogAggregator.scala
@@ -0,0 +1,48 @@
+package io.snappydata.aggr
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.streaming.SnappyStreamingContext
+import org.apache.spark.sql.{SaveMode, SnappyContext}
+import org.apache.spark.streaming.Milliseconds
+
+object SnappyLogAggregator extends App {
+
+  val sparkConf = new org.apache.spark.SparkConf()
+    .setAppName("logAggregator")
+    .setMaster("snappydata://localhost:10334")
+
+  val sc = new SparkContext(sparkConf)
+  val snsc = SnappyStreamingContext(SnappyContext.getOrCreate(sc), Milliseconds(10000))
+
+  snsc.sql("drop table if exists impressionlog")
+  snsc.sql("drop table if exists snappyStoreTable")
+
+  snsc.sql("create stream table impressionlog (timestamp long, publisher string," +
+    " advertiser string, " +
+    "website string, geo string, bid double, cookie string) " +
+    "using directkafka_stream options " +
+    "(storagelevel 'MEMORY_AND_DISK_SER_2', " +
+    "rowConverter 'io.snappydata.aggr.KafkaStreamToRowsConverter' ," +
+    " kafkaParams 'metadata.broker.list->localhost:9092,localhost:9093'," +
+    " topics 'adnetwork-topic'," +
+    " K 'java.lang.String'," +
+    " V 'io.snappydata.aggr.ImpressionLog', " +
+    " KD 'kafka.serializer.StringDecoder', " +
+    " VD 'io.snappydata.aggr.ImpressionLogAvroDecoder')")
+
+  snsc.sql("create table snappyStoreTable(publisher string," +
+    " geo string, avg_bid double, imps long, uniques long) " +
+    "using column " +
+    "options(PARTITION_BY 'publisher')")
+
+  snsc.registerCQ("select publisher, geo, avg(bid) as avg_bid, count(*) imps, count(distinct(cookie)) uniques" +
+    " from impressionlog window (duration '10' seconds, slide '10' seconds)" +
+    " where geo != 'unknown' group by publisher, geo")
+    .foreachDataFrame(df => {
+      df.show
+      df.write.format("column").mode(SaveMode.Append)
+        .options(Map.empty[String, String]).saveAsTable("snappyStoreTable")
+    })
+  snsc.start
+  snsc.awaitTermination
+}
