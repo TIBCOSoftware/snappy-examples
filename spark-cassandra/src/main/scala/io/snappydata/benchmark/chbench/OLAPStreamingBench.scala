c5c7ffd6c106a49a79df00d0aa8d9097affb2e83
ymahajan <ymahajan@snappydata.io>
2016-05-08 21:27:16 +0530

    combining olap+streaming to share sparkcontext

--- spark-cassandra/src/main/scala/io/snappydata/benchmark/chbench/OLAPStreamingBench.scala
+++ spark-cassandra/src/main/scala/io/snappydata/benchmark/chbench/OLAPStreamingBench.scala
@@ -10,21 +10,31 @@ import org.apache.spark.sql.types.{IntegerType, StructType, TimestampType}
 import org.apache.spark.streaming.{Duration, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
-object StreamingBench extends App {
+object OLAPStreamingBench extends App {
   val rootLogger = Logger.getLogger("org");
   rootLogger.setLevel(Level.ERROR);
 
   val conf = new SparkConf(true)
     .setAppName(getClass.getSimpleName)
-    .set("spark.cassandra.connection.host", "127.0.0.1")
+    //.set("spark.cassandra.connection.host", "127.0.0.1")
+    .set("spark.cassandra.connection.host" , "172.31.13.52")
     .set("spark.cassandra.auth.username", "cassandra")
     .set("spark.cassandra.auth.password", "cassandra")
     .set("spark.cassandra.sql.keyspace", "tpcc")
-    .setMaster("local[*]")
+    .set("spark.driver.memory", "28g")
+    .set("spark.executor.memory","28g")
+    //.setMaster("local[*]")
+    .setMaster("spark://ip-172-31-13-48:7077")
+
+  val assemblyJar = System.getenv("PROJECT_ASSEMBLY_JAR")
+  if (assemblyJar != null) {
+    conf.set("spark.driver.extraClassPath", assemblyJar)
+    conf.set("spark.executor.extraClassPath", assemblyJar)
+  }
 
   val sc = new SparkContext(conf)
   val cc = new CassandraSQLContext(sc)
-  val ssc = new StreamingContext(sc, Duration(10000))
+  val ssc = new StreamingContext(sc, Duration(2000))
 
   CassandraConnector(conf).withSessionDo { session =>
     println("********CONNECTED TO CASSANDRA V8  *************")
@@ -46,7 +56,7 @@ object StreamingBench extends App {
   val rows = stream.map(v => Row(v.w_id,
     v.d_id, v.c_id, v.i_id, v.c_ts, new java.sql.Timestamp(System.currentTimeMillis)))
 
-  val window_rows = rows.window(new Duration(20*1000), new Duration(20*1000))
+  val window_rows = rows.window(new Duration(20 * 1000), new Duration(20 * 1000))
 
   window_rows.foreachRDD(rdd => {
     val df = cc.createDataFrame(rdd, schema)
@@ -78,5 +88,59 @@ object StreamingBench extends App {
   })
 
   ssc.start
+
+  def getCurrentDirectory = new java.io.File(".").getCanonicalPath
+
+  // scalastyle:off println
+
+  var i: Int = 0
+  while (i < 4) {
+    val outFileName = s"HQueries-${i}.out"
+
+    val pw = new PrintWriter(outFileName)
+
+    i = i + 1
+    for (q <- HQueries.queries) {
+      val start: Long = System.currentTimeMillis
+      try {
+        q._1 match {
+          case "Q11" =>
+            val ret = cc.sql(HQueries.Q11a).collect()
+            assert(ret.length == 1)
+            val paramVal = ret(0).getDecimal(0)
+            val qry = q._2.replace("?", paramVal.toString)
+            cc.sql(qry).collect()
+          case "Q15" =>
+            var ret = cc.sql(HQueries.Q15a)
+            ret.registerTempTable("revenue")
+            val maxV = cc.sql(HQueries.Q15b).collect()
+            val paramVal = maxV(0).getDouble(0)
+            val qry = q._2.replace("?", paramVal.toString)
+            cc.sql(qry).collect()
+          case "Q22" =>
+            val ret = cc.sql(HQueries.Q22a).collect()
+            assert(ret.length == 1)
+            val paramVal = ret(0).getDouble(0)
+            val qry = q._2.replace("?", paramVal.toString)
+            cc.sql(qry).collect()
+          case "Q16" | "Q20" | "Q21" =>
+            pw.println("Not running " + q._1)
+            pw.flush()
+          //cc.sql(q._2).collect()
+          case _ =>
+            cc.sql(q._2).collect()
+        }
+      } catch {
+        case e: Throwable => pw.println(s"Exception for query ${q._1}:  " + e)
+          pw.println(e.getStackTraceString)
+      }
+      val end: Long = System.currentTimeMillis - start
+      pw.println(s"${new java.util.Date(System.currentTimeMillis())} Time taken by ${q._1} is $end")
+      pw.flush()
+    }
+    pw.close()
+  }
+  //  // Return the output file name
+  //  s"See ${getCurrentDirectory}"
   ssc.awaitTermination
 }
